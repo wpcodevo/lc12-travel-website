58bc1b6cdf1e3fde914c648e130924e22e9dcf0b
Edem <ziddah.edem@gmail.com>
2021-07-13 20:42:52 +0000

    about to finish

---
+++ js/index.js
@@ -0,0 +1,108 @@
+// Navigation Toggle
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("open");
+});
+
+// Discount Media
+const video = document.querySelector(".video");
+const button = document.querySelector(".video-control");
+
+button.addEventListener("click", playpausevideo);
+
+function playpausevideo() {
+  if (video.paused) {
+    button.innerHTML = "<i class='bx bx-pause' ></i>";
+    video.play();
+  } else {
+    button.innerHTML = "<i class='bx bx-play' ></i>";
+    video.pause();
+  }
+}
+
+// Preloader
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 2000);
+});
+
+// Fix Nav
+const navigation = document.querySelector(".navigation");
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+  if (scrollHeight > 200) {
+    navigation.classList.add("fix");
+    header.style.zIndex = "1000";
+  } else {
+    navigation.classList.remove("fix");
+  }
+});
+
+// Scroll
+const links = document.querySelectorAll(".nav-link");
+
+Array.from(links).map((link) => {
+  link.addEventListener("click", (e) => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navigation.getBoundingClientRect().height;
+    const fix = navigation.classList.contains("fix");
+    let position = element.offsetTop - navHeight;
+
+    // if (!fix) {
+    //   position = position - navHeight;
+    // }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    navList.classList.remove("open");
+  });
+});
+
+// Scroll Reveal
+
+const scroll = ScrollReveal({
+  distance: "100px",
+  duration: 2500,
+  reset: true,
+});
+
+scroll.reveal(`.content h1, .content .btn`, {
+  origin: "top",
+  interval: 100,
+});
+
+scroll.reveal(`.about .col h1, .about .col p, .about .col .btn`, {
+  origin: "left",
+  interval: 150,
+});
+
+scroll.reveal(
+  `.about .col:last-child,.contact .location,.more .col:last-child,.newsletter .form`,
+  {
+    origin: "right",
+  }
+);
+
+scroll.reveal(`.service img,.contact .title`, {
+  origin: "top",
+});
+
+scroll.reveal(`.service .col,.trip .row`, {
+  origin: "bottom",
+});
+
+scroll.reveal(`.trip .title,.more .col:first-child,.newsletter .col`, {
+  origin: "left",
+});
---
+++ js/slider.js
@@ -0,0 +1,37 @@
+const swiper1 = new Swiper(".slider-1", {
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
+
+const swiper2 = new Swiper(".slider-2", {
+  autoHeight: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiper3 = new Swiper(".slider-3", {
+  autoHeight: true,
+  effect: "coverflow",
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 40,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".custom-next",
+    prevEl: ".custom-prev",
+  },
+  pagination: {
+    el: ".custom-pagination",
+  },
+});
