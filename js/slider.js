87e93ad3de3f48b6026e312e2b69cb8d88f886a8
Edem <ziddah.edem@gmail.com>
2021-07-14 07:39:47 +0000

    :tada:

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,9 @@
 const swiper1 = new Swiper(".slider-1", {
+  // autoplay: {
+  //   delay: 3500,
+  //   disableOnInteraction: false,
+  // },
+  loop: true,
   navigation: {
     nextEl: ".swiper-next",
     prevEl: ".swiper-prev",
